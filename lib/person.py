937052ad9887a30bbabe9e6b8ef1147b4a128881
NdegeFelix <findech6@gmail.com>
2023-12-01 23:32:13 +0300

    completed assignment

--- lib/person.py
+++ lib/person.py
@@ -5,3 +5,13 @@ class Person:
 
     #Instance method definition
     pass
+    def walk(self):
+        print("The person is walking.")
+    def talk(self):
+        print("Hello World!")
+
+
+    pass
+person1 = Person()
+person1.talk()
+person1.walk()
